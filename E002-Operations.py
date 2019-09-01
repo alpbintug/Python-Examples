3845974db3ced8cb0ca1adcc6704dbabffc37029
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-09-01 21:47:14 +0300

    Update E002-Operations.py

--- E002-Operations.py
+++ E002-Operations.py
@@ -1,7 +1,17 @@
-a = 10;
-b = 6;
+a = 10
+b = 6
+#Arithmetic Operators
 print(a/b) #Division, Output: 1.6666666666666667
 print(a*b) #Multiplication, Output: 60
 print(a%b) #Modulus, Output: 4
 print(b**2) #Exponent, Output: 36
 print(a//b) #Floor division, Output: 1
+b**=0.5 #Short writing for b = b^(1/2)
+print(b) #Output: 2.449489742783178
+b**=2 # b = b^2
+print(b)#Output: 5.999999999999999
+b=6
+
+#Logic operators
+print(a!=b) #Not equal, Output: True
+print(a==b) #Equal, Output: False
