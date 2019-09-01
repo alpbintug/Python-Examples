67d206484f033fcc0d92528299b527ccea2c46ee
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-09-01 21:37:01 +0300

    Update E002-Operations.py

--- E002-Operations.py
+++ E002-Operations.py
@@ -1,5 +1,7 @@
 a = 10;
 b = 6;
-print(a/b) #Output: 1.6666666666666667
-print(a*b) #Output: 60
-print(a%b) #Output: 4
+print(a/b) #Division, Output: 1.6666666666666667
+print(a*b) #Multiplication, Output: 60
+print(a%b) #Modulus, Output: 4
+print(b**2) #Exponent, Output: 36
+print(a//b) #Floor division, Output: 1
