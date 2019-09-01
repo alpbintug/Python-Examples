3ea4ad2a721ebea6b22f4a7ae45ea13462bf74db
Alp Bintuğ Uzun <48649717+alpbintug@users.noreply.github.com>
2019-09-01 22:02:34 +0300

    Create E003-Variables.py

---
+++ E003-Variables.py
@@ -0,0 +1,12 @@
+i = 10 #Integer
+print(i) #Output: 10
+f = 10.0 #Floating point
+print(f) #Output: 10.0
+s = "String" #String
+print(s) #Output: String
+
+a,b,c,d = 1,2,3.4,"Five" #Multiple assigment
+print(a) #Output: 1
+print(b) #Output: 2
+print(c) #Output: 3.4
+print(d) #Output: Five
